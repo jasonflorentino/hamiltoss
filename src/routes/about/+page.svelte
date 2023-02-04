51e6a3099c4679824a696af4a8c06c955522abf9
Jason Florentino <jasonmflorentino@gmail.com>
2023-02-03 23:28:31 -0600

    update About

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -14,10 +14,10 @@
 <main class="p-5">
 	<h1 class="text-4xl md:text-5xl lg:text-6xl font-bold text-emerald-300 mb-4">About</h1>
 
-	<div class="text-3xl md:text-4xl font-light max-w-3xl">
+	<div class="text-2xl md:text-3xl lg:text-4xl font-light max-w-3xl">
 		<p class="leading-relaxed">
 			Easily find if something is recyclable in Hamilton. More information, and the source of this
-			data, can be found at
+			data, can be found on
 			<a
 				href={hamiltonUrl}
 				target="_blank"
