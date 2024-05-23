2e4c8fe5d6e97c250cf4489a1e5bd72206c65d6b
Jason Florentino <jasonmflorentino@gmail.com>
2024-05-22 22:47:33 -0400

    Update dates

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -41,67 +41,69 @@ export const SEARCH_SUGGESTIONS = [
 export const PICKUP_CHANGES: SpecialPickupDetail[] = [
 	{
 		name: 'Good Friday',
-		date: '2023-04-07',
+		date: '2024-03-29',
 		affectedPeriod: 'day',
 	},
 	{
 		name: 'Easter Monday',
-		date: '2023-04-10',
+		date: '2024-04-01',
 		affectedPeriod: 'none',
 	},
 	{
 		name: 'Victoria Day',
-		date: '2023-05-22',
+		date: '2024-05-20',
 		affectedPeriod: 'week',
 	},
 	{
 		name: 'Canada Day',
-		date: '2023-07-01',
-		affectedPeriod: 'none',
+		date: '2024-07-01',
+		affectedPeriod: 'week',
 	},
 	{
 		name: 'Civic Holiday',
-		date: '2023-08-07',
+		date: '2024-08-05',
 		affectedPeriod: 'week',
 	},
 	{
 		name: 'Labour Day',
-		date: '2023-09-04',
+		date: '2024-09-02',
 		affectedPeriod: 'week',
 	},
 	{
 		name: 'Thanksgiving',
-		date: '2023-10-09',
+		date: '2024-10-14',
 		affectedPeriod: 'week',
 	},
 	{
 		name: 'Yard Waste',
-		date: '2023-12-08',
+		date: '2024-12-06',
 		affectedPeriod: 'yard',
 	},
 	{
 		name: 'Christmas & Boxing Day',
-		date: '2023-12-25',
+		date: '2024-12-25',
+		// TODO: one day later if normally wed,thu,fri
 		affectedPeriod: 'week',
 	},
 	{
 		name: 'New Years Day',
-		date: '2024-01-01',
+		date: '2025-01-01',
+		// TODO: one day later if normally wed,thu,fri
 		affectedPeriod: 'week',
 	},
 	{
 		name: 'Family Day',
-		date: '2024-02-19',
+		date: '2025-02-17',
 		affectedPeriod: 'week',
 	},
 	{
 		name: 'Yard Waste',
-		date: '2024-03-04',
+		date: '2025-03-03',
 		affectedPeriod: 'yard',
 	},
 	{
 		name: 'Good Friday',
-		date: '2024-03-29',
+		date: '2025-04-18',
 		affectedPeriod: 'day',
 	},
 ];
