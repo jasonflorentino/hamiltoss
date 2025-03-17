a2d6d532a043426e2a4e029741a829f7e9b09be2
Jason Florentino <jason.florentino@relayfi.com>
2025-03-16 22:14:49 -0400

    update important dates for 2025/26

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -41,67 +41,62 @@ export const SEARCH_SUGGESTIONS = [
 export const PICKUP_CHANGES: SpecialPickupDetail[] = [
 	{
 		name: 'Good Friday',
-		date: '2024-03-29',
+		date: '2025-04-18',
 		affectedPeriod: 'day',
 	},
 	{
 		name: 'Easter Monday',
-		date: '2024-04-01',
+		date: '2025-04-21',
 		affectedPeriod: 'none',
 	},
 	{
 		name: 'Victoria Day',
-		date: '2024-05-20',
+		date: '2025-05-19',
 		affectedPeriod: 'week',
 	},
 	{
 		name: 'Canada Day',
-		date: '2024-07-01',
-		affectedPeriod: 'week',
+		date: '2025-07-01',
+		affectedPeriod: ['tue', 'wed', 'thu', 'fri'],
 	},
 	{
 		name: 'Civic Holiday',
-		date: '2024-08-05',
+		date: '2025-08-04',
 		affectedPeriod: 'week',
 	},
 	{
 		name: 'Labour Day',
-		date: '2024-09-02',
+		date: '2025-09-01',
 		affectedPeriod: 'week',
 	},
 	{
 		name: 'Thanksgiving',
-		date: '2024-10-14',
+		date: '2025-10-13',
 		affectedPeriod: 'week',
 	},
 	{
 		name: 'Yard Waste',
-		date: '2024-12-06',
+		date: '2025-12-01',
 		affectedPeriod: 'yard',
 	},
 	{
 		name: 'Christmas & Boxing Day',
-		date: '2024-12-25',
-		affectedPeriod: ['wed', 'thu', 'fri'],
+		date: '2025-12-25',
+		affectedPeriod: ['thu', 'fri'],
 	},
 	{
 		name: 'New Years Day',
-		date: '2025-01-01',
-		affectedPeriod: ['wed', 'thu', 'fri'],
+		date: '2026-01-01',
+		affectedPeriod: ['thu', 'fri'],
 	},
 	{
 		name: 'Family Day',
-		date: '2025-02-17',
+		date: '2026-02-16',
 		affectedPeriod: 'week',
 	},
 	{
 		name: 'Yard Waste',
-		date: '2025-03-03',
+		date: '2026-03-02',
 		affectedPeriod: 'yard',
-	},
-	{
-		name: 'Good Friday',
-		date: '2025-04-18',
-		affectedPeriod: 'day',
 	},
 ];
